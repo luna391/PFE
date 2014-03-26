59bc5b3bdc7739dfc927ee5d8f70ea43231dc668
luna391 <benabdallah.kmar@gmail.com>
2014-03-26 16:01:12 +0100

    routing urls

--- pfe_v1/urls.py
+++ pfe_v1/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from accounts.forms import SignupFormExtra
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pfe_v1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$','userena.views.signup',{'signup_form': SignupFormExtra}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^accounts/', include('userena.urls')),
